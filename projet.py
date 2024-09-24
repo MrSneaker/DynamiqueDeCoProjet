f9397f107188af78841bcd1d4a6fa2e132bc56c0
Loric <loric.gros@gmail.com>
2024-09-24 12:03:08 +0200

    rules (to modify)

--- projet.py
+++ projet.py
@@ -23,3 +23,38 @@ class Literals:
     
     def __hash__(self):
       return hash((self.name, self.neg))
+    
+class Rules:
+
+    num_regle = 0
+
+    def __init__(self,s1,s2,b) -> None:
+        self.name = Rules.setRef()
+        self.premises = s1
+        self.conclusion = s2
+        self.defeasible = b
+
+    def __str__(self) -> str:
+        premises_str = ', '.join(map(str, self.premises))
+        #conclusion_str = ', '.join(map(str, self.conclusion))
+        conclusion_str = self.conclusion
+        if self.defeasible:
+            return f"r{self.name} : [{premises_str}] => [{conclusion_str}]"
+        return f"r{self.name} : [{premises_str}] -> [{conclusion_str}]"
+    
+    @classmethod
+    def setRef(cls):
+        cls.num_regle += 1
+        return cls.num_regle
+    
+    def __eq__(self, other):
+        if isinstance(other, Rules):
+            return (
+                self.premises == other.premises and 
+                self.conclusion == other.conclusion and 
+                self.defeasible == other.defeasible
+            )
+        return False
+    
+    def __hash__(self):
+      return hash((self.conclusion, self.defeasible)) * hash(tuple(self.premises))
