da18019cdec78d844f215b7f92fc0f921276f893
Munoz Mateo <mateo.munoz@etu.univ-lyon1.fr>
2024-09-30 16:19:48 +0200

    TODO: bien noter les regles utilisée et les premisses des arguments inférer en seconde instance.

--- projet.py
+++ projet.py
@@ -1,5 +1,7 @@
-from itertools import product
+from itertools import chain, product
 import os
+
+from cv2 import compare
 
 class Literals:
     
@@ -8,7 +10,7 @@ class Literals:
         self.neg = neg
 
     def __str__(self) -> str:
-        if self.neg:
+        if not self.neg:
             return f"{self.name}"
         else:
             return f"!{self.name}"
@@ -28,7 +30,7 @@ class Rules:
 
     num_regle = 0
 
-    def __init__(self,s1,s2,b) -> None:
+    def __init__(self,s1,s2) -> None:
         self.name = Rules.setRef()
         self.premises = s1
         self.conclusion = s2
@@ -73,6 +75,12 @@ class Argument:
     
     def get_rules_used(self) -> set[Rules]:
         return self.rules_used
+    
+    def __str__(self) -> str:
+        premises_str = ', '.join(map(str, self.premises)) if self.premises else "None"
+        conclusion_str = str(self.conclusion)
+        rules_str = ', '.join(map(str, self.rules_used)) if self.rules_used else "None"
+        return f"Argument:\n  Premises: [{premises_str}]\n  Conclusion: {conclusion_str}\n  Rules used: [{rules_str}]"
   
 
 class ArgumentsFinder:
@@ -84,15 +92,52 @@ class ArgumentsFinder:
     
     def compute_arguments(self) -> set[Argument]:
         computed_args = set()
+        rules_usable = self.rules
         
-        for rule in self.rules:
-            if rule.premises in self.assumptions or rule.premises is None:
-                arg_premises = self.assumptions.union(rule.premises)
-                computed_args.add(Argument(premises=arg_premises, conclusion=rule.conclusion, rules_used=set(rule)))
+        rules_used = []
+        for rule in rules_usable:
+            print(f'rule is {rule}')
+            rule_activable = len(self.assumptions.intersection(rule.premises)) > 0
+            if rule_activable or len(rule.premises) == 0:
+                arg_premises = self.assumptions.intersection(rule.premises)
+                new_arg = Argument(premises=arg_premises, conclusion=rule.conclusion, rules_used=set([rule]))
+                computed_args.add(new_arg)
+                rules_used.append(rule)
+        rules_usable.difference_update(rules_used)
+                
+        for assumption in self.assumptions:
+            computed_args.add(Argument(set([assumption]), assumption, None))
+        
+        old_args = computed_args.copy()
+        new_args = computed_args.copy()
+        while True:
+            rules_used = []
+            for rule in rules_usable:
+                rule_activable = len(set([arg.get_conclusion() for arg in old_args]).intersection(rule.premises))
+                if rule_activable or len(rule.premises) == 0:
+                    arg_premises = self.assumptions.intersection(rule.premises)
+                    new_arg = Argument(premises=arg_premises, conclusion=rule.conclusion, rules_used=set([rule]))
+                    new_args.add(new_arg)
+                    rules_used.append(rule)
+            rules_usable.difference_update(set(rules_used))
+            print(f'len old is {len(old_args)}, len new is {len(new_args)}')
+            if len(old_args) == len(new_args):
+                break
+            old_args = new_args
+        computed_args = new_args
+        
         return computed_args
                 
-        
-        
+    
 if __name__ == "__main__":
-    langage = set[Literals]('a','b','c','q','p','r','s','t')
-    rules = set[Rules](Rules(set(Literals())))
+    langage = set[Literals]([Literals('a', None), Literals('b', None), Literals('c', None),Literals('q', None), Literals('p', None), Literals('r', None),Literals('s', None), Literals('t', None)])
+    assumptions = set[Literals]([Literals('a', None), Literals('b', None), Literals('c', None)])
+    rules = set[Rules]([Rules(set([Literals('q', None), Literals('a', None)]), Literals('p', None)), Rules(set(), Literals('q', None)), Rules(set([Literals('b', None), Literals('c', None)]), Literals('r', None)), Rules(set([Literals('p', None), Literals('c', None)]), Literals('t', None)), Rules(set([Literals('t', None)]), Literals('s', None))])
+    
+    arg_finder = ArgumentsFinder(langage, assumptions, rules)
+    args = arg_finder.compute_arguments()
+    
+    for arg in args:
+        print(arg)
+        
+    print(f'number of args is {len(args)}')
